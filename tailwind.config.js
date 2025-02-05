dbdf204c6ccb0377dabd224f5d9b171a6e5fa38a
thaqiulislamkafi <thaqiulislamkafi@tuta.io>
2025-02-05 21:57:54 +0600

    Arrival and Offers section updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: theme => ({
+          'gradient-to-45': 
+              'linear-gradient(45deg, #53DB3A, #FFFFFF)',
+          'gradient-to-135': 
+              'linear-gradient(135deg, #ffed4a, #ff3860)',
+          // You can add more custom classes here
+        })
+      },
     },
     plugins: [],
   }
